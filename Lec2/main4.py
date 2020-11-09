5841271c342641b1454ae26a93360cca2e81ca2a
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-11-09 13:04:24 +0300

    added final lec1-lec2

---
+++ Lec2/main4.py
@@ -0,0 +1,9 @@
+"""
+D1:F solution
+"""
+
+name = input("Enter name: ")
+lastname = input("Enter lastname: ")
+age = input("Enter age: ")
+
+print("Имя: " + name + " ,",      "Фамилия: " + lastname + " ,",       "Возраст: " + age + " .",          "Студент BPS")
